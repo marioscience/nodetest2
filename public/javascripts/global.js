8dd71a77e09c7db009bc911fd0f6df1ecf9d8ced
mmatos <marioscience@hotmail.com>
2017-01-17 16:43:43 -0700

    Added delete user funcing

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -10,6 +10,8 @@ $(document).ready(function () {
     populateTable();
 
     $('#userList table tbody').on('click', 'td a.linkshowuser', showUserInfo);
+    $('#btnAddUser').on('click', addUser);
+    $('#userList table tbody').on('click', 'td a.linkdeleteuser', deleteUser);
 });
 
 // Functions
@@ -55,4 +57,80 @@ function showUserInfo(event) {
     $('#userInfoAge').text(thisUserObject.age);
     $('#userInfoGender').text(thisUserObject.gender);
     $('#userInfoLocation').text(thisUserObject.location);
+}
+
+// Add User
+function addUser (event) {
+    event.preventDefault();
+
+    // Super basic validation - increase error count variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function (index, val) {
+        if ($(this).val() === '') {errorCount++}
+    });
+
+    // Check and amek sure errorCount's still at zero
+    if (errorCount === 0) {
+        // If it is, compile all user info into one object
+        var newUser = {
+            'username': $('#addUser fieldset input#inputUserName').val(),
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'location': $('#addUser fieldset input#inputUserLocation').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val()
+        };
+
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function (response) {
+
+            // Check for succesful blank response
+            if (response.msg === '') {
+                // Clear form inputs
+                $('#addUser fieldset input').val('');
+
+                //Update Table
+                populateTable();
+            } else {
+                // If something goes wrong, alert the error message
+                alert("Error: " + response.msg);
+            }
+        });
+    } else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+}
+
+function deleteUser (event) {
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm("Yes! you DO want to delete that record. It was stupid anyways.");
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+        // If they did
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function (response) {
+            // Check for success
+            if (response.msg === '') {
+            } else {
+                alert('Error: ' + response.msg)
+            }
+
+            // Update the table
+            populateTable();
+        });
+    } else {
+        // Do Nothing if they said no
+        return false;
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,26 @@ router.get('/userlist', function (req, res) {
   })
 });
 
+/* POST to Add User */
+router.post('/adduser', function (req, res) {
+    var db = req.db;
+    var userlist = db.get('userlist');
+
+    userlist.insert(req.body, function (err, result) {
+        res.send(
+            (err === null) ? {msg: ''} : {msg: err}
+        );
+    });
+});
+
+/* DELETE our users */
+router.delete('/deleteuser/:id', function (req, res) {
+    var db = req.db;
+    var userlist = db.get('userlist');
+    var userToDelete = req.params.id;
+    userlist.remove({'_id': userToDelete}, function (err) {
+        res.send((err === null) ? { msg: '' } : { msg: 'error: ' + err })
+    });
+});
+
 module.exports = router;
